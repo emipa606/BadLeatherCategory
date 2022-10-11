f66831bf42ab055a39e42e75f575ccc5f1fdba2a
Mlie <emipa606@users.noreply.github.com>
2022-10-11 22:28:05 +0200

    Mod updated for 1.4 and passed autotests

--- Source/BadLeatherCategory/BadLeatherCategory.cs
+++ Source/BadLeatherCategory/BadLeatherCategory.cs
@@ -1,64 +1,67 @@
 ﻿using System.Linq;
 using Verse;
 
-namespace BadLeatherCategory
+namespace BadLeatherCategory;
+
+[StaticConstructorOnStartup]
+public class BadLeatherCategory
 {
-    [StaticConstructorOnStartup]
-    public class BadLeatherCategory
+    static BadLeatherCategory()
     {
-        static BadLeatherCategory()
+        var LeathersCategory = DefDatabase<ThingCategoryDef>.GetNamedSilentFail("Leathers");
+        if (LeathersCategory == null)
         {
-            var LeathersCategory = DefDatabase<ThingCategoryDef>.GetNamedSilentFail("Leathers");
-            if (LeathersCategory == null)
-            {
-                Log.ErrorOnce("[BadLeatherCategory]: Could not find the Leathers-category. Will not sort bad leather.",
-                    "LeathersCategory".GetHashCode());
-                return;
-            }
-
-            var LeatherBadCategory = DefDatabase<ThingCategoryDef>.GetNamedSilentFail("LeatherBad");
-            if (LeatherBadCategory == null)
-            {
-                Log.ErrorOnce(
-                    "[BadLeatherCategory]: Could not find the LeatherBad-category. Will not sort bad leather.",
-                    "LeatherBadCategory".GetHashCode());
-                return;
-            }
-
-            var counter = 0;
-            foreach (var raceDef in from raceDef in DefDatabase<ThingDef>.AllDefsListForReading
-                where raceDef.race is {Humanlike: true}
-                select raceDef)
-            {
-                if (raceDef.race.leatherDef == null)
-                {
-                    continue;
-                }
-
-                if (LeatherBadCategory.childThingDefs.Contains(raceDef.race.leatherDef))
-                {
-                    continue;
-                }
-
-                if ((from race in DefDatabase<ThingDef>.AllDefsListForReading
-                    where race.race is {Humanlike: false} && race.race.leatherDef == raceDef.race.leatherDef
-                    select race).Any())
-                {
-                    continue;
-                }
-
-                LeathersCategory.childThingDefs.Remove(raceDef.race.leatherDef);
-                raceDef.race.leatherDef.thingCategories.Remove(LeathersCategory);
-                LeatherBadCategory.childThingDefs.Add(raceDef.race.leatherDef);
-                raceDef.race.leatherDef.thingCategories.Add(LeatherBadCategory);
-                counter++;
-            }
-
-            LeathersCategory.ClearCachedData();
-            LeatherBadCategory.ClearCachedData();
-            LeathersCategory.ResolveReferences();
-            LeatherBadCategory.ResolveReferences();
-            Log.Message($"[BadLeatherCategory]: Moved {counter} leather to the Bad Leather-category");
+            Log.ErrorOnce("[BadLeatherCategory]: Could not find the Leathers-category. Will not sort bad leather.",
+                "LeathersCategory".GetHashCode());
+            return;
         }
+
+        var LeatherBadCategory = DefDatabase<ThingCategoryDef>.GetNamedSilentFail("LeatherBad");
+        if (LeatherBadCategory == null)
+        {
+            Log.ErrorOnce(
+                "[BadLeatherCategory]: Could not find the LeatherBad-category. Will not sort bad leather.",
+                "LeatherBadCategory".GetHashCode());
+            return;
+        }
+
+        var counter = 0;
+        foreach (var raceDef in from raceDef in DefDatabase<ThingDef>.AllDefsListForReading
+                 where raceDef.race is { Humanlike: true }
+                 select raceDef)
+        {
+            if (raceDef.race.leatherDef == null)
+            {
+                //Log.Message($"{raceDef} has no leather");
+                continue;
+            }
+
+            if (LeatherBadCategory.childThingDefs.Contains(raceDef.race.leatherDef))
+            {
+                //Log.Message($"{raceDef.race.leatherDef} already moved");
+                continue;
+            }
+
+            if ((from race in DefDatabase<ThingDef>.AllDefsListForReading
+                    where race.race is { Humanlike: false } && race.race.leatherDef == raceDef.race.leatherDef
+                    select race).Any())
+            {
+                //Log.Message($"{raceDef.race.leatherDef} also used by non-humanlike pawns. Skipping");
+                continue;
+            }
+
+            //Log.Message($"Addeing {raceDef.race.leatherDef} from {raceDef} as bad");
+            LeathersCategory.childThingDefs.Remove(raceDef.race.leatherDef);
+            raceDef.race.leatherDef.thingCategories.Remove(LeathersCategory);
+            LeatherBadCategory.childThingDefs.Add(raceDef.race.leatherDef);
+            raceDef.race.leatherDef.thingCategories.Add(LeatherBadCategory);
+            counter++;
+        }
+
+        LeathersCategory.ClearCachedData();
+        LeatherBadCategory.ClearCachedData();
+        LeathersCategory.ResolveReferences();
+        LeatherBadCategory.ResolveReferences();
+        Log.Message($"[BadLeatherCategory]: Moved {counter} leather to the Bad Leather-category");
     }
 }
